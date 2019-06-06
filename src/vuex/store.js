3024f5f19fa07e372d0a79a18fd44502f586e7f2
liangzhu <1678881243@qq.com>
2019-06-07 00:29:33 +0800

    test

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -8,19 +8,9 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    userInfo: {}, // 用户信息
-
-    personInfo: {}, // 个人资料
-
     categoryList: {}, // 首页分类列表
 
-    cartCommodityVOList: {}, // 购物车列表
-    buyList: [], // 要下单的商品uuid
-    buyReceiverUuid: '', // 下单-收获人uuid
-
     orderCommodityVOList: {}, // 订单列表
-    orderCommodityUuid: '', // 要付款订单的uuid
-    sumPrice: 0, // 需要支付金额
 
     commodityList: {}, // 商品列表
     vCommodityInfoUuid: "", // 正在查看的商品uuid
@@ -38,21 +28,9 @@ export default new Vuex.Store({
     jpageSize: 12,
     jmaxPageNum: 1,
 
-    consigneeList: {}, // 收货人信息列表
-    consigneeInfo: {}, // 收货人信息
-
     token: '', // 请求令牌
     is_login: false, // 是否登录
     signUpStep: 0, // 注册步骤
-
-
-
-    errorpopupSwitch: false, // 错误弹窗开关
-    addSwitch: false, // 添加钻石开关
-    addAddressSwitch: false, // 添加收货地址开关
-    cPwsSwitch: false, // 修改密码开关
-    cInfoSwitch: false, // 修改个人资料开关
-    cRASwitch: false, // 修改收货地址开关
     errorMess: ''
   },
   getters,
